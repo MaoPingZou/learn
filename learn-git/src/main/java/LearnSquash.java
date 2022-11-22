4dfe422dce8d16e6564e55119f3a4e3062fcee8b
zoumaoping <zoulao749@gamil.com>
2022-11-22 20:14:03 +0800

    learn-git 子模块初始化；

---
+++ learn-git/src/main/java/LearnSquash.java
@@ -0,0 +1,16 @@
+/**
+ * @author MaoPing Zou
+ * @date 2022/11/22 20:12
+ */
+public class LearnSquash {
+
+    /**
+     * firstField
+     */
+    private static final String firstField = "firstField";
+
+    /**
+     * secondField
+     */
+    private static final String secondField = "secondField";
+}
